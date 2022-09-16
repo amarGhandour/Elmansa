f426317ff4acc2f5e5fef09543141ffaeada9cd1
mora <amarghandour89@gmail.com>
2022-09-16 11:50:38 +0200

    login, register, and course show pages.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
+//
+// $(document).ready(function () {
+//     nice scroll
+//     $("#video-sidebar").niceScroll({
+//         zindex: 9999
+//     });
+//
+//     drop down
+//     $('.dropdown-btn').click(function (){
+//         $(this).siblings('div.dropdown-container').toggleClass('d-none')
+//     });
+// });
 
-// const closeBtn = document.querySelector('.close-btn');
+// navbar
 const openBtn = document.querySelector('.open-btn');
 
-let nav = document.querySelectorAll('.nav');
+let nav = document.querySelectorAll('.burger-nav');
 
 openBtn.addEventListener('click', () => {
-   nav.forEach((navEl) => {
-       navEl.classList.toggle('visible');
-   })
+    nav.forEach((navEl) => {
+        navEl.classList.toggle('visible');
+    })
 });
 
 let navWrapper = document.querySelector('.nav-wrapper');
@@ -16,17 +28,33 @@ window.addEventListener('scroll', fixedNav)
 
 function fixedNav() {
     console.log(navWrapper.offsetHeight)
-    if (window.scrollY > navWrapper.offsetHeight){
+    if (window.scrollY > navWrapper.offsetHeight) {
         navWrapper.classList.add('active')
 
-    }else {
+    } else {
         navWrapper.classList.remove('active')
 
     }
 }
 
-// closeBtn.addEventListener('click', () => {
-//     nav.forEach((navEl) => {
-//         navEl.classList.remove('visible');
-//     })
-// });
+// dropdown
+let dorpdownBtn = document.querySelectorAll('.dropdown-btn');
+
+dorpdownBtn.forEach((btn) => {
+    btn.addEventListener('click', function (event) {
+        this.nextElementSibling.classList.toggle('d-none');
+    });
+});
+
+
+// video navigation mobile
+let cardHeaders = document.querySelectorAll('.video-nav-mobile .card-header');
+
+cardHeaders.forEach((btn) => [
+    btn.addEventListener('click', function (event) {
+        this.nextElementSibling.classList.toggle('d-none');
+        this.querySelector('.fa-chevron-down').classList.toggle('up')
+    })
+])
+
+
